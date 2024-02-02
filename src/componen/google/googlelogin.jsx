debeadba9ddd8bd7e4fa349bd978c808c563eb79
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-02 16:05:11 +0100

    test logout 4

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { GoogleLogin, GoogleLogout } from '@react-oauth/google';
 
 function Login() {
 	const [isLoggedIn, setLoggedIn] = useState(false);
-
-	const userName = response.profileObj.name;
-	const userProfileImage = response.profileObj.imageUrl;
+	const [userName, setUserName] = useState('');
+	const [userProfileImage, setUserProfileImage] = useState('');
 
 	const handleLoginSuccess = (response) => {
 		console.log('Login success:', response);
+
+		// Estrarre informazioni dell'utente
+		const name = response?.profileObj?.name;
+		const imageUrl = response?.profileObj?.imageUrl;
+
+		// Aggiornare lo stato con le informazioni dell'utente
+		setUserName(name || '');
+		setUserProfileImage(imageUrl || '');
+
+		// Impostare lo stato di accesso a true
 		setLoggedIn(true);
 	};
 
@@ -16,21 +25,29 @@ function Login() {
 		console.log('Login error:', error);
 	};
 
-	const logga = () => {
+	const handleLogout = () => {
+		console.log('Logout');
 		setLoggedIn(false);
-		googleLogout();
+		setUserName('');
+		setUserProfileImage('');
+		// Altre azioni di logout personalizzate possono essere aggiunte qui
 	};
 
 	return (
 		<div>
-			<select className="p-2 bg-white rounded-xl flex flex-row justify-between w-[200px] h-[35px]">
-				<img src={userProfileImage} className="w-[20px] h-[20px]" />
-				<p className="text-bold">{userName}</p>
-				<option onClick={logga} className="text-red-600 text-bold">
-					Logout
-				</option>
-			</select>
-			<GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError} buttonText="Accedi con Google" />
+			{isLoggedIn && (
+				<select className="p-2 bg-white rounded-xl flex flex-row justify-between w-[200px] h-[35px]">
+					<img src={userProfileImage} className="w-[20px] h-[20px]" alt="User Profile" />
+					<p className="text-bold">{userName}</p>
+					<GoogleLogout onLogoutSuccess={handleLogout}>
+						<option className="text-red-600 text-bold">Logout</option>
+					</GoogleLogout>
+				</select>
+			)}
+
+			{!isLoggedIn && (
+				<GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError} buttonText="Accedi con Google" />
+			)}
 		</div>
 	);
 }
