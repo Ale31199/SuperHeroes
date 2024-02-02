880c228eafa5c648ca50428d150ced878ae944a3
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-03 00:58:44 +0100

    prova con google analitycs

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
@@ -6,6 +6,7 @@ function Login() {
 	const [isLoggedIn, setLoggedIn] = useState(false);
 	const [userName, setUserName] = useState('');
 	const [userProfileImage, setUserProfileImage] = useState('');
+	const [analyticsAccountName, setAnalyticsAccountName] = useState('');
 
 	const handleLoginSuccess = async (response) => {
 		console.log('Login success:', response);
@@ -15,13 +16,28 @@ function Login() {
 		setLoggedIn(true);
 		if (accessToken) {
 			try {
+				// Fetch user info from Google Login
 				const name = response.profileObj.getName();
 				const imageUrl = response.profileObj.imageUrl;
 
 				setUserName(name || '');
 				setUserProfileImage(imageUrl || '');
+
+				// Fetch Google Analytics account information
+				const analyticsAccountResponse = await axios.get(
+					'https://www.googleapis.com/analytics/v3/management/accounts/~all',
+					{
+						headers: {
+							Authorization: `Bearer ${accessToken}`,
+						},
+					}
+				);
+
+				// Assuming you want the name of the first account in the list
+				const firstAnalyticsAccountName = analyticsAccountResponse.data.items[0].name;
+				setAnalyticsAccountName(firstAnalyticsAccountName || '');
 			} catch (error) {
-				console.error('Error fetching user info:', error);
+				console.error('Error fetching user or analytics info:', error);
 			}
 		}
 	};
@@ -35,7 +51,8 @@ function Login() {
 		setLoggedIn(false);
 		setUserName('');
 		setUserProfileImage('');
-		// Altre azioni di logout personalizzate possono essere aggiunte qui
+		setAnalyticsAccountName('');
+		// Other custom logout actions can be added here
 	};
 
 	return (
@@ -46,7 +63,9 @@ function Login() {
 						<img src={userProfileImage} className="w-[20px] h-[20px]" alt="User Profile" />
 						<p className="text-bold">{userName}</p>
 					</div>
-
+					<div>
+						<p className="text-bold">{analyticsAccountName}</p>
+					</div>
 					<button onClick={handleLogout} className="text-white bg-red-900 text-sm p-1 rounded-xl text-bold">
 						Logout
 					</button>
