3f5728450eef1ae9b63b7ac3d62c677806888d00
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-02 17:08:01 +0100

    test 8

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -23,8 +23,15 @@ function Login() {
 
 	const decodeJwt = (token) => {
 		const base64Url = token.split('.')[1];
-		const base64 = base64Url.replace('-', '+').replace('_', '/');
-		return JSON.parse(atob(base64));
+		const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+		const jsonPayload = decodeURIComponent(
+			atob(base64)
+				.split('')
+				.map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+				.join('')
+		);
+
+		return JSON.parse(jsonPayload);
 	};
 
 	const handleLoginError = (error) => {
