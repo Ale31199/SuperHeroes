8594961b3503ffbc073453b3731c6029e4ec597f
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-02 15:25:53 +0100

    aggiunto logout forse

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -1,20 +1,39 @@
-import { GoogleLogin, useGoogleOneTapLogin } from '@react-oauth/google';
+import React, { useState } from 'react';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 
 function Login() {
-	// Metodo 1: usando il componente GoogleLogin
-	const handleSuccess = (response) => {
+	const [isLoggedIn, setLoggedIn] = useState(false);
+
+	const handleLoginSuccess = (response) => {
 		console.log('Login success:', response);
+		setLoggedIn(true);
 	};
 
-	const handleError = (error) => {
+	const handleLoginError = (error) => {
 		console.log('Login error:', error);
 	};
 
-	return (
-		<div>
-			<GoogleLogin onSuccess={handleSuccess} onError={handleError} buttonText="Accedi con Google" />
-		</div>
-	);
+	const handleLogout = () => {
+		console.log('Logout');
+		setLoggedIn(false);
+		googleLogout();
+		// Altre azioni di logout personalizzate possono essere aggiunte qui
+	};
+
+	if (isLoggedIn) {
+		return (
+			<div>
+				<p>Utente loggato</p>
+				<GoogleLogin onSuccess={handleLogout} buttonText="Logout" />
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError} buttonText="Accedi con Google" />
+			</div>
+		);
+	}
 }
 
 export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import App from './App.jsx';
 import './App.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const ClientID = '854917209460-shi180ck4md6fp9f2picmevsooarjm8t.apps.googleusercontent.com';
+const clientID = '854917209460-shi180ck4md6fp9f2picmevsooarjm8t.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<GoogleOAuthProvider clientId={ClientID}>
+		<GoogleOAuthProvider clientId={clientID}>
 			<App />
 		</GoogleOAuthProvider>
 	</React.StrictMode>
