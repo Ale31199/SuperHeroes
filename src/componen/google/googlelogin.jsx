67842d3a02ab85ba47fc0f59d8552af246483d02
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-04 18:45:19 +0100

    test 25

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -1,53 +1,81 @@
-import { useState } from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
 	const [isLoggedIn, setLoggedIn] = useState(false);
 	const [userInfo, setUserInfo] = useState(null);
 
-	const handleLoginSuccess = (response) => {
-		console.log('Login success:', response);
-		setLoggedIn(true);
-		const profile = response.getBasicProfile();
+	const handleSignIn = (googleUser) => {
+		const profile = googleUser.getBasicProfile();
 		setUserInfo({
 			id: profile.getId(),
 			name: profile.getName(),
 			imageUrl: profile.getImageUrl(),
 			email: profile.getEmail(),
 		});
+		setLoggedIn(true);
 	};
 
-	const handleLoginError = (error) => {
-		console.log('Login error:', error);
+	const handleSignOut = () => {
+		const auth2 = window.gapi.auth2.getAuthInstance();
+		auth2.signOut().then(() => {
+			console.log('User signed out.');
+			setLoggedIn(false);
+		});
 	};
 
-	const handleLogout = () => {
-		console.log('Logout');
-		setLoggedIn(false);
+	const initGoogleSignIn = () => {
+		window.gapi.load('auth2', () => {
+			window.gapi.auth2
+				.init({
+					client_id: '854917209460-shi180ck4md6fp9f2picmevsooarjm8t.apps.googleusercontent.com',
+				})
+				.then(() => {
+					const auth2 = window.gapi.auth2.getAuthInstance();
+
+					// Check if the user is already signed in
+					if (auth2.isSignedIn.get()) {
+						const googleUser = auth2.currentUser.get();
+						handleSignIn(googleUser);
+					}
+				});
+		});
+	};
+
+	useEffect(() => {
+		initGoogleSignIn();
+	}, []);
+
+	const handleSignInClick = () => {
+		const auth2 = window.gapi.auth2.getAuthInstance();
+		auth2.signIn().then((googleUser) => {
+			handleSignIn(googleUser);
+		});
+	};
+
+	const handleSignOutClick = () => {
+		handleSignOut();
 	};
 
 	return (
 		<div>
-			{isLoggedIn && (
+			{isLoggedIn ? (
 				<div className="p-2 bg-white rounded-xl flex flex-row justify-between items-center w-[200px] h-[35px]">
 					<div className="w-[170px] flex justify-start gap-x-3">
 						<img src={userInfo.imageUrl} className="w-[20px] h-[20px]" alt="User Profile" />
 						<p className="text-bold">{userInfo.name}</p>
 					</div>
-					<button onClick={handleLogout} className="text-white bg-red-900 text-sm p-1 rounded-xl text-bold">
+					<button onClick={handleSignOutClick} className="text-white bg-red-900 text-sm p-1 rounded-xl text-bold">
 						Logout
 					</button>
 				</div>
+			) : (
+				<button onClick={handleSignInClick} className="text-white bg-blue-500 text-sm p-2 rounded-xl text-bold">
+					Accedi con Google
+				</button>
 			)}
 
-			{!isLoggedIn && (
-				<GoogleLogin
-					className="g-signin2"
-					onSuccess={handleLoginSuccess}
-					onError={handleLoginError}
-					buttonText="Accedi con Google"
-				/>
-			)}
+			<script src="https://apis.google.com/js/platform.js" async defer></script>
+			<script dangerouslySetInnerHTML={{ __html: 'initGoogleSignIn();' }} />
 		</div>
 	);
 }
