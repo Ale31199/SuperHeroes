6cff6d0d4aa1e6d909fa8ad19bfbeb018162f274
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-05 15:17:19 +0100

    test local

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -11,9 +11,13 @@ function Login() {
 		if (salvaLogin) {
 			setLoggedIn(JSON.parse(salvaLogin));
 		}
-		const salvaNome = localStorage.getItem('userInfo');
+		const salvaNome = localStorage.getItem('userInfo.name');
 		if (salvaNome) {
 			setUserInfo(JSON.parse(salvaNome));
+		}
+		const salvaImg = localStorage.getItem('userInfo.imageUrl');
+		if (salvaImg) {
+			setUserInfo(JSON.parse(salvaImg));
 		}
 	}, []);
 
@@ -22,7 +26,8 @@ function Login() {
 		console.log('Login success:', deco);
 		setLoggedIn(true);
 		localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
-		localStorage.setItem('userInfo', JSON.stringify(userInfo));
+		localStorage.setItem('userInfo.name', JSON.stringify(userInfo.name));
+		localStorage.setItem('userInfo.imageUrl', JSON.stringify(userInfo.imageUrl));
 
 		setUserInfo({
 			name: deco.name,
@@ -43,7 +48,7 @@ function Login() {
 	return (
 		<div>
 			{isLoggedIn && (
-				<div className="p-3 bg-slate-800 border-2 border-white rounded-xl flex flex-row justify-between items-center w-fit h-[35px]">
+				<div className="p-3 bg-slate-800 border-2 border-white rounded-xl flex flex-row justify-between items-center w-fit h-[45px]">
 					<div className="w-[170px] flex justify-start items-center gap-x-3">
 						<img src={userInfo.imageUrl} className="w-[30px] h-[30px] rounded-full" alt="User Profile" />
 						<p className="font-bold text-white text-sm text-center">{userInfo.name}</p>
