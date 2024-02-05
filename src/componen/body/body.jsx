66eb0058e5ca8a822cccb6020e0e08f3017a82e3
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-05 16:44:27 +0100

    test di prova

--- src/componen/body/body.jsx
+++ src/componen/body/body.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import sfondo from '/src/img/sfondo2.jpg';
 import video from '/src/img/videotest.mp4';
+import add from '/src/img/add.png';
+import like from '/src/img/heart.png';
+import comment from '/src/img/editing.png';
 
 const boody = () => {
 	const [isLoggedIn, setLoggedIn] = useState(false);
@@ -25,7 +28,33 @@ const boody = () => {
 				<p className="text-white absolute text-lg md:text-xl top-[200px]"></p>
 				<img src={sfondo} className="opacity-30 blur-[2px] object-cover md:scale-125 transition-all duration-500 " />
 			</div>
-			<div className="bg-black bg-opacity-95 rounded-xl absolute top-[200px] w-[95%] h-fit gap-y-10 p-4 md:w-[85%] grid xl:grid-cols-3  md:grid-cols-2 grid-cols-1 justify-items-center items-center transition-all duration-500  ">
+
+			{!isLoggedIn && (
+				<div
+					className={`text-white flex-col w-[100%] absolute top-[130px] items-center justify-center text-5xl ${
+						isLoggedIn ? 'flex' : 'flex'
+					}`}
+				>
+					Buongiorno
+					<img src={userInfo.imageUrl} className="w-[40px] h-[40px] rounded-full ml-2 mt-3" />
+					<p className="text-white font-bold text-sm md:text-base">{userInfo.name}</p>
+				</div>
+			)}
+
+			<div className="bg-slate-900 bg-opacity-95 rounded-xl absolute top-[280px] w-[95%] h-[65px] gap-y-10 p-4 md:w-[85%] flex justify-center items-center transition-all duration-500">
+				<input
+					placeholder="Search something..."
+					className="p-3 w-[50%] h-[50px] outline-none text-white bg-neutral-800 rounded-xl"
+				/>
+				<div className="flex flex-row w-[40%] items-center">
+					<div className="absolute right-[5%] w-[130px] scale-75 md:scale-90 transition-all duration-500 flex flex-row items-center justify-center cursor-pointer hover:to-orange-400 p-2 bg-gradient-to-t from-red-900 to-orange-700 rounded-xl">
+						<img src={add} className="w-[40px] h-[40px] invert" />
+						<p className="text-white ml-2">Post</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="bg-black bg-opacity-95 rounded-xl absolute top-[350px] w-[95%] h-fit gap-y-10 p-4 md:w-[85%] grid xl:grid-cols-3  md:grid-cols-2 grid-cols-1 justify-items-center items-center transition-all duration-500  ">
 				<div className=" bg-slate-900 cursor-pointer relative w-[95%] rounded-xl h-fit transition-all duration-500">
 					<div className="w-[100%] h-full overflow-hidden justify-center">
 						<img
@@ -42,32 +71,6 @@ const boody = () => {
 							<button className="hover:text-teal-400">Like 12</button>
 							<button className="hover:text-teal-400">Comments 345</button>
 						</div>
-					</div>
-				</div>
-
-				<div className=" bg-slate-900 cursor-pointer  w-[95%] rounded-xl h-fit transition-all duration-500">
-					<div className="flex flex-col justify-start p-2 md:p-3 gap-y-3 items-center">
-						<p className="text-teal-400 font-bold text-start relative ">{userInfo.name}</p>
-						<p className="text-white font-bold text-start relative text-sm md:text-base  w-[90%]">
-							Bello sto sito ci sto perdendo le oreeee hahahhaah
-						</p>
-						<div className="text-violet-600 font-bold text-sm md:text-base w-full flex justify-between p-2 items-center">
-							<button className="hover:text-teal-400">Like 23</button>
-							<button className="hover:text-teal-400">Comments 14</button>
-						</div>
-					</div>
-				</div>
-				<div className=" bg-slate-900 cursor-pointer  w-[95%] rounded-xl h-fit transition-all duration-500">
-					<div className="w-full h-full overflow-hidden">
-						<video src={video} controls className="object-cover h-[350px] rounded-xl " />
-					</div>
-					<div className="flex flex-col justify-start p-2 md:p-3 gap-y-3 items-center">
-						<p className="text-teal-400 font-bold text-start relative ">{userInfo.name}</p>
-						<p className="text-white font-bold text-start relative text-sm md:text-base  w-[90%]">Welcome To The DCC</p>
-					</div>
-					<div className="text-violet-600 font-bold text-sm md:text-base w-full flex justify-between p-2 items-center">
-						<button className="hover:text-teal-400">Like 123</button>
-						<button className="hover:text-teal-400">Comments 5430</button>
 					</div>
 				</div>
 			</div>
