ba361f9d60c5f8d388f01187026c1c13f5c58493
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-05 13:58:54 +0100

    test login

--- src/componen/google/googlelogin.jsx
+++ src/componen/google/googlelogin.jsx
@@ -1,48 +1,52 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 
 function Login() {
 	const [isLoggedIn, setLoggedIn] = useState(false);
 	const [userInfo, setUserInfo] = useState(null);
 
-	const handleSignIn = (googleUser) => {
-		const profile = googleUser.getBasicProfile();
-		setUserInfo({
-			id: profile.getId(),
-			name: profile.getName(),
-			imageUrl: profile.getImageUrl(),
-			email: profile.getEmail(),
-		});
+	const handleLoginSuccess = (response) => {
+		console.log('Login success:', response);
 		setLoggedIn(true);
+
+		// Accedi direttamente alle informazioni dell'utente dalla risposta
+		setUserInfo({
+			id: response.googleId,
+			name: response.profileObj.name,
+			imageUrl: response.profileObj.imageUrl,
+			email: response.profileObj.email,
+		});
 	};
 
-	const handleSignOut = (googleUser) => {
-		googleUser.signOut().then(() => {
-			console.log('User signed out.');
-			setLoggedIn(false);
-		});
+	const handleLoginError = (error) => {
+		console.log('Login error:', error);
+	};
+
+	const handleLogout = () => {
+		console.log('Logout');
+		setLoggedIn(false);
 	};
 
 	return (
 		<div>
-			{isLoggedIn ? (
+			{isLoggedIn && (
 				<div className="p-2 bg-white rounded-xl flex flex-row justify-between items-center w-[200px] h-[35px]">
 					<div className="w-[170px] flex justify-start gap-x-3">
 						<img src={userInfo.imageUrl} className="w-[20px] h-[20px]" alt="User Profile" />
 						<p className="text-bold">{userInfo.name}</p>
 					</div>
-					<button onClick={handleSignOut} className="text-white bg-red-900 text-sm p-1 rounded-xl text-bold">
+					<button onClick={handleLogout} className="text-white bg-red-900 text-sm p-1 rounded-xl text-bold">
 						Logout
 					</button>
 				</div>
-			) : (
+			)}
+
+			{!isLoggedIn && (
 				<GoogleLogin
-					clientId="854917209460-shi180ck4md6fp9f2picmevsooarjm8t.apps.googleusercontent.com"
-					scope="profile email"
-					onSuccess={handleSignIn}
-					buttonProps={{
-						className: 'text-white bg-blue-500 text-sm p-2 rounded-xl text-bold',
-					}}
+					className="g-signin2"
+					onSuccess={handleLoginSuccess}
+					onError={handleLoginError}
+					buttonText="Accedi con Google"
 				/>
 			)}
 		</div>
