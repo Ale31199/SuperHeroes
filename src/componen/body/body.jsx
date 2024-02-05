6966bbd8e15be7d2cba7bc9f2c9e561e9ce064a8
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-05 17:23:54 +0100

    test  prova 2

--- src/componen/body/body.jsx
+++ src/componen/body/body.jsx
@@ -29,7 +29,7 @@ const boody = () => {
 				<img src={sfondo} className="opacity-30 blur-[2px] object-cover md:scale-125 transition-all duration-500 " />
 			</div>
 
-			{!isLoggedIn && (
+			{isLoggedIn && (
 				<div
 					className={`text-white flex-col w-[100%] flex absolute top-[130px] items-center justify-center text-5xl ${
 						isLoggedIn ? 'visible' : 'invisible'
@@ -41,58 +41,54 @@ const boody = () => {
 				</div>
 			)}
 
-			{!isLoggedIn && (
-				<div
-					className={`bg-neutral-900 border-2 border-neutral-700 bg-opacity-95 rounded-xl absolute w-[95%] h-[65px] gap-y-10 p-4 md:w-[85%] flex justify-center items-center transition-all duration-500 ${
-						isLoggedIn ? 'top-[280px]' : 'top-[110px]'
+			<div
+				className={`bg-neutral-900 border-2 border-neutral-700 bg-opacity-95 rounded-xl absolute w-[95%] h-[65px] gap-y-10 p-4 md:w-[85%] flex justify-center items-center transition-all duration-500 ${
+					isLoggedIn ? 'top-[280px]' : 'top-[110px]'
+				}`}
+			>
+				<input
+					placeholder="Search something..."
+					className="p-3 w-[50%] h-[50px] outline-none text-white bg-neutral-950 rounded-xl"
+				/>
+				<button
+					disabled={!isLoggedIn}
+					className={`flex flex-row w-[40%] items-center ${
+						isLoggedIn ? 'opacity-100' : 'opacity-35 cursor-not-allowed '
 					}`}
 				>
-					<input
-						placeholder="Search something..."
-						className="p-3 w-[50%] h-[50px] outline-none text-white bg-neutral-950 rounded-xl"
-					/>
-					<button
-						disabled={!isLoggedIn}
-						className={`flex flex-row w-[40%] items-center ${
-							isLoggedIn ? 'opacity-100' : 'opacity-35 cursor-not-allowed '
-						}`}
-					>
-						<div className="absolute right-[5%] w-[130px] scale-75 md:scale-90 transition-all duration-500 flex flex-row items-center justify-center cursor-pointer hover:to-orange-400 p-2 bg-gradient-to-t from-red-900 to-orange-700 rounded-xl">
-							<img src={add} className="w-[40px] h-[40px] invert" />
-							<p className="text-white ml-2">Post</p>
-						</div>
-					</button>
-				</div>
-			)}
+					<div className="absolute right-[5%] w-[130px] scale-75 md:scale-90 transition-all duration-500 flex flex-row items-center justify-center cursor-pointer hover:to-orange-400 p-2 bg-gradient-to-t from-red-900 to-orange-700 rounded-xl">
+						<img src={add} className="w-[40px] h-[40px] invert" />
+						<p className="text-white ml-2">Post</p>
+					</div>
+				</button>
+			</div>
 
-			{!isLoggedIn && (
-				<div
-					className={`bg-neutral-900 border-2 border-neutral-700 bg-opacity-95 rounded-xl absolute w-[95%] h-fit gap-y-10 p-4 md:w-[85%] grid xl:grid-cols-3  md:grid-cols-2 grid-cols-1 justify-items-center items-center transition-all duration-500 ${
-						isLoggedIn ? 'top-[350px]' : 'top-[180px]'
-					}`}
-				>
-					<div className=" bg-black cursor-pointer relative w-[95%] rounded-xl h-fit transition-all duration-500">
-						<div className="w-[100%] h-full overflow-hidden justify-center">
-							<img
-								src={sfondo}
-								className="object-cover w-[100%] h-[350px] rounded-t-xl hover:scale-125 transition-all duration-500"
-							/>
-						</div>
-						<div className="flex flex-col justify-start p-2 md:p-3 gap-y-3 items-center">
-							<p className="text-teal-400 font-bold text-start relative ">{userInfo.name}</p>
-							<p className="text-white font-bold text-start relative text-sm md:text-base  w-[90%]">
-								I supereroi sono fichissimi guardate! Ho fatto questa fan art per tutti voi spero vi piaccia!
-							</p>
-							<div className="text-green-600 font-bold text-sm md:text-base w-full flex justify-between p-2 items-center">
-								<button className="hover:text-teal-400 flex flex-row items-center">
-									<img src={like} className="w-[30px] h-[30px] invert mr-2 animate-bounce " /> 12
-								</button>
-								<button className="hover:text-teal-400">Comments 345</button>
-							</div>
+			<div
+				className={`bg-neutral-900 visible border-2 border-neutral-700 bg-opacity-95 rounded-xl absolute w-[95%] h-fit gap-y-10 p-4 md:w-[85%] grid xl:grid-cols-3  md:grid-cols-2 grid-cols-1 justify-items-center items-center transition-all duration-500 ${
+					isLoggedIn ? 'top-[350px]' : 'top-[180px]'
+				}`}
+			>
+				<div className=" bg-black cursor-pointer relative w-[95%] rounded-xl h-fit transition-all duration-500">
+					<div className="w-[100%] h-full overflow-hidden justify-center">
+						<img
+							src={sfondo}
+							className="object-cover w-[100%] h-[350px] rounded-t-xl hover:scale-125 transition-all duration-500"
+						/>
+					</div>
+					<div className="flex flex-col justify-start p-2 md:p-3 gap-y-3 items-center">
+						<p className="text-teal-400 font-bold text-start relative ">{userInfo.name}</p>
+						<p className="text-white font-bold text-start relative text-sm md:text-base  w-[90%]">
+							I supereroi sono fichissimi guardate! Ho fatto questa fan art per tutti voi spero vi piaccia!
+						</p>
+						<div className="text-green-600 font-bold text-sm md:text-base w-full flex justify-between p-2 items-center">
+							<button className="hover:text-teal-400 flex flex-row items-center">
+								<img src={like} className="w-[30px] h-[30px] invert mr-2 animate-bounce " /> 12
+							</button>
+							<button className="hover:text-teal-400">Comments 345</button>
 						</div>
 					</div>
 				</div>
-			)}
+			</div>
 		</>
 	);
 };
