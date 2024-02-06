c14441acefc6e22dee5c52a3f8dd7b4983d97487
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-06 22:14:32 +0100

    test

--- src/componen/body/body.jsx
+++ src/componen/body/body.jsx
@@ -80,7 +80,7 @@ const boody = ({ firebaseApp }) => {
 			descr: post.descr,
 			likes: 0,
 			comments: 0,
-			id: userInfo.id,
+			id: userInfo.email,
 		};
 
 		const docRef = await addDoc(collection(db, 'posts'), newPost);
@@ -92,6 +92,7 @@ const boody = ({ firebaseApp }) => {
 			descr: '',
 			likes: 0,
 			comments: 0,
+			id: userInfo.email,
 		});
 
 		location.reload();
