97fb45fbc37451a06ff5f33314be4596024bc40b
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-02-06 13:08:39 +0100

    test 4 posting

--- src/componen/body/body.jsx
+++ src/componen/body/body.jsx
@@ -14,7 +14,7 @@ const boody = () => {
 	const [feed, setFeed] = useState([]);
 	const [descr, setDescr] = useState('');
 	const [post, setPost] = useState({
-		image: selectedFile,
+		image: URL.createObjectURL(selectedFile),
 		descr: '',
 		likes: 0,
 		comments: 0,
